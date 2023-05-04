3a235f0e7830f44ff819454c3e4c3f3941f162db
LOsevOD <lazar.aleks21@yandex.ru>
2023-05-04 11:48:36 +0300

    CALC-1: fix maven dep.

--- src/main/java/com/github/losevod/calculator/Calculator.java
+++ src/main/java/com/github/losevod/calculator/Calculator.java
@@ -2,7 +2,6 @@ package com.github.losevod.calculator;
 
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
